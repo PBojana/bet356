c8a685ee60a364b0386372e550349187a43f7707
PBojana <bo_sale@hotmail.com>
2017-02-20 13:09:07 +0100

    Commit 7

--- src/test/java/bet356/bet356/LigaUtakmica.java
+++ src/test/java/bet356/bet356/LigaUtakmica.java
@@ -28,15 +28,15 @@ public class LigaUtakmica {
 	public void setUp() throws Exception {
 		 
 		baseUrl="https://www.bet365.com/?&cb=10325414067#/HO/";
-		liga1="Northern Ireland Reserve League";
-		utakmica1="Glentoran Reserves v Coleraine Reserves";
+		///liga1="Northern Ireland Reserve League";
+		//utakmica1="Glentoran Reserves v Coleraine Reserves";
 		
 	}
 
 	@Test
 	public void test() { 
  
-		File file = new File("C:/Users/Administrator/Desktop/bet365.properties");
+		File file = new File("C:/Users/Administrator/bet365.properties");
     	FileInputStream fileInput = null;
 		try {
 			fileInput = new FileInputStream(file);
@@ -55,21 +55,26 @@ public class LigaUtakmica {
 			
 	    	WebDriver driver = new FirefoxDriver();
 	    	driver.get(baseUrl);
-	    	driver.findElement(By.linkText("English")).click();
+	    	Wait.seconds(10);
 	    	driver.findElement(By.linkText("English")).click();
 	    	Wait.seconds(10);
-	    	driver.findElement(By.xpath("//div[25]")).click();
+	     	driver.findElement(By.linkText("English")).click();
+	    	Wait.seconds(10);
+	    	driver.findElement(By.xpath("//*[text()='Soccer']")).click();
+	    	//driver.findElement(By.cssSelector("div.wn-Classification")).click();
+	    	//(//a[contains(text(),'Save')])[2]")).click();
+	    	
 	    	Wait.seconds(10);
 	     	//Biras liga
-	    	 driver.findElement(By.xpath("//*[text()='"+prop.getProperty("liga1")+"']")).click();
+	    	 driver.findElement(By.xpath("//*[text()='"+prop.getProperty("liga")+"']")).click();
 	    	Wait.seconds(5);
 	    	//Biras utakmica
-	    	driver.findElement(By.xpath("//*[text()='"+prop.getProperty("utakmica1")+"']")).click();
+	    	driver.findElement(By.xpath("//*[text()='"+prop.getProperty("utakmica")+"']")).click();
 	    	Wait.seconds(5);
 
 	    	String imaMain=driver.findElement(By.cssSelector("div.wl-NavBarScroller_HScroll")).getText();
-	    	assertTrue("Ne Postoi Asian Lines za "+prop.getProperty("utakmica1")+" ",imaMain.contains("Asian Lines"));
-
+	    	assertTrue("Ne Postoi Asian Lines za "+prop.getProperty("utakmica")+" ",imaMain.contains("Asian Lines"));
+	    	System.out.println("Postoi Asian Lines za "+prop.getProperty("utakmica"));
 
 
 }
